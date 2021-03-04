61dc7a02ec4a6dcbacf5c1facbae8409f33d4748
lvyongc <2431043403@qq.com>
2021-03-04 15:39:35 +0800

    two

--- cnode/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import {createStore,combineReducers} from "redux";
-import topics from "./reducer/topics";
-import topic from "./reducer/topic";
-import user from "./reducer/user";
-export default createStore(combineReducers({
-    topics,
-    topic,
-    user
-}));
--- cnode/src/view/page404/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-function Page404() {
-  return (<div>页面飞走了</div>);
-}
-
-export default Page404;
